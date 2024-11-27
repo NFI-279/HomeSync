d24bc8acab647c9ef526ab499f039484bac6d753
NFI <78752610+NFI-279@users.noreply.github.com>
2024-11-27 19:23:22 +0200

    Updated tawkto.js

--- js/tawkto.js
+++ js/tawkto.js
@@ -1,1 +1,9 @@
-
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/674750964304e3196ae96e3e/1idn95j79';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
